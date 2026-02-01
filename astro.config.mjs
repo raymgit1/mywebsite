e56645199b1333d4dffa1d23efcd4cb9671dda47
Raymond Gu <s.raymondgu@gmail.com>
2026-02-02 09:32:30 +1100

    Website - Raymond

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,14 +6,19 @@ import { autoNewTabExternalLinks } from './src/autoNewTabExternalLinks';
 
 import partytown from "@astrojs/partytown";
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://devolio.devaradise.com',
   integrations: [mdx(), sitemap(), tailwind(), partytown()],
+
   markdown: {
     extendDefaultPlugins: true,
     rehypePlugins: [[autoNewTabExternalLinks, {
       domain: 'localhost:4321'
     }]]
-  }
+  },
+
+  adapter: cloudflare()
 });
